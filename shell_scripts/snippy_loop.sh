fad06f300bd2c591075af4f792a739db411f61b0
maralohde <mara.lohde@gmx.de>
2022-02-25 09:12:43 +0000

    renamed loop for filtering clean genomes after sourmash analysis and added loop for automated snippy analysis and result overview

---
+++ shell_scripts/snippy_loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for X in *.fasta
+do
+	SAMPLE_NAME=$(echo "$X" ls | cut -f 1-2 -d "_") #with technology
+	SAMPLE=$(echo "$X" ls | cut -f 1 -d "_") #without_technology
+	snippy snippy --cpus 8 --outdir "$SAMPLE_NAME" --ref "$SAMPLE"_ill_*.fasta --ctgs  "$SAMPLE_NAME"*.fasta
+	echo "Sample: "$SAMPLE_NAME"" >> "$SAMPLE_NAME"_variants.txt
+	echo "Reference: "$SAMPLE"_ill" >> "$SAMPLE_NAME"_variants.txt
+	cat "$SAMPLE_NAME"/*.txt| grep "Variant" >> "$SAMPLE_NAME"_variants.txt
+	mkdir snippy_variants | mv "$SAMPLE_NAME"_variants.txt snippy_variants/
+done
