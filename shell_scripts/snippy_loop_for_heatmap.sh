d686ed48cd65e99f40376efb55c4256c4f9d5d09
Mara Lohde <67577191+maralohde@users.noreply.github.com>
2022-05-12 14:33:37 +0200

    Create snippy_loop_for_heatmap.sh

---
+++ shell_scripts/snippy_loop_for_heatmap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run snippy for all files in 2 dirs 
+# output: .csv with ref;ctgs;SNPS
+
+for X in reference/*.fasta
+do
+        echo "__________Ref: $X"
+        for Y in ctgs/*.fasta
+        do
+        snippy --cpus 20 --outdir ${X%.fasta}_vs_${Y%.fasta} --ref $X --ctgs $Y
+        VALUE=$(grep "Variant-SNP" ${X%.fasta}_vs_${Y%.fasta}/snps.txt | cut -f2)
+        echo "$X;$Y;$VALUE" >> SNP_results_1.csv
+        done
+done 
