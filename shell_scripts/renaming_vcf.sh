c66c572764ec23dd5f9f47ed28378696e4011a47
Mara Lohde <67577191+maralohde@users.noreply.github.com>
2022-06-01 15:32:15 +0200

    Update renaming_vcf.sh

--- shell_scripts/renaming_vcf.sh
+++ shell_scripts/renaming_vcf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# excecute in environment of snippy directorys
 # add directory name to snps.vcf files
 
-for f in */snps.vcf
+for f in */snps.*
     do fp=$(dirname "$f")
     fl=$(basename "$f")
     mv "$fp/$fl" "$fp/$fp"_"$fl"; 
