e36c9aad626715d3a552e37c8766a7daf82ff4a5
Mara Lohde <67577191+maralohde@users.noreply.github.com>
2023-06-22 11:31:52 +0200

    Create mapping_reads.sh

---
+++ shell_scripts/mapping_reads.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CPUS="20"
+DIR="BAMS"
+mkdir -p "${DIR}"
+fasta_dir=$1
+fastq_dir=$2
+
+# Loop over Files in fasta dir
+for fasta in "$fasta_dir"/*.fasta; do
+    # extract basename
+    fasta_name=$(basename "$fasta")
+    fasta_name="${fasta_name%.fasta}"  # remove file ending
+
+    # Loop over Files in fastq dir
+    for fastq in "$fastq_dir"/*.fastq.gz; do
+        # extract basename
+        fastq_name=$(basename "$fastq")
+        fastq_name="${fastq_name%.fastq.gz}"  # remove file ending
+
+        # Compare basenames of fasta and fastq files
+        if [ "$fasta_name" = "$fastq_name" ]; then
+            echo "Mapping $fastq reads on $fasta"
+            minimap2 -ax map-ont ${fasta} ${fastq} | samtools view -bS - | samtools sort -@ ${CPUS} - > ${DIR}/${fasta_name}.bam
+          	chmod 666 ${DIR}/${fasta_name}.bam
+        fi
+    done
+done
